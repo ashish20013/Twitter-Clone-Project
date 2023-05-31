a2eb5222cc86685a37ab02f1274f14df74d5d1fe
vikas3207 <vikaspiplodiya562@gmail.com>
2023-05-31 16:00:49 +0530

    New singup registeration page

---
+++ src/components/singUpRegister/dobData.js
@@ -0,0 +1,54 @@
+export const months = [
+    {
+     id: 1,
+      label: 'January',
+    },
+    {
+     id: 2,
+      label: 'February',
+    },
+    {
+     id: 3,
+      label: 'March',
+    },
+    {
+     id: 4,
+      label: 'April',
+    },
+    {
+      id: 5,
+       label: 'May',
+     },
+     {
+      id: 6,
+       label: 'June',
+     },
+     {
+      id: 7,
+       label: 'July',
+     },
+     {
+      id: 8,
+       label: 'August',
+     },
+     {
+      id: 9,
+       label: 'September',
+     },
+     {
+      id: 10,
+       label: 'October',
+     },
+     {
+      id: 11,
+       label: 'November',
+     },
+     {
+      id: 12,
+       label: 'December',
+     },
+  ];
+
+  export const days = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
+
+  export const years = [1990,1991,1992,1993,1994,1995,1996,1997,1999,2000,2001,2002,2003,2004,2005]
