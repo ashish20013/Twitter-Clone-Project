ae501b8b0073097f161b03a72644116d9e9d1057
vikas3207 <vikaspiplodiya562@gmail.com>
2023-06-02 03:54:30 +0530

    Final Code with latest update

--- src/App.js
+++ src/App.js
@@ -4,23 +4,22 @@ import Singup from './components/singUpRegister/SingUpRegister';
 import './App.css';
 import SignIn from './components/singInPage/signin';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomeLayout from '../src/components/homePage/HomeLayout'
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<SignIn />} />
-          <Route path="/signUp" element={<SignUp />} />
-          <Route path="/singup" element={<Singup />} />
-        </Routes>
+    // <BrowserRouter>
+    //   <div className="App">
+    //     <Routes>
+    //       <Route path="/" element={<SignIn />} />
+    //       <Route path="/signUp" element={<SignUp />} />
+    //       <Route path="/singup" element={<Singup />} />
+    //     </Routes>
 
-
-
-
-      </div>
-    </BrowserRouter>
+    //  </div>
+    // </BrowserRouter>
+    <HomeLayout />
   );
 }
 
--- src/components/homePage/HomeLayout.js
+++ src/components/homePage/HomeLayout.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './homeLayout.css'
 import LeftSide from './LeftSide'
 import RightSide from './RightSide'
-import {BsTwitter,BsHash,BsBookmark} from 'react-icons/bs'
-import {AiTwotoneHome,AiOutlineMail} from 'react-icons/ai'
-import {IoMdNotificationsOutline} from 'react-icons/io'
-import {CiViewList,CiCircleMore} from 'react-icons/ci'
-import {FaTwitterSquare} from 'react-icons/fa'
-import {ImProfile} from 'react-icons/im'
-import {FiMoreHorizontal} from 'react-icons/fi'
+import { BsChat } from 'react-icons/bs'
+import { AiOutlineRetweet, AiOutlineHeart } from 'react-icons/ai'
+import { MdIosShare } from 'react-icons/md'
+import { data } from './ApiData'
+
+
 
 const HomeLayout = () => {
+    const [like, setLike] = useState(0)
+
     return (
         <section id="home-layout">
             <LeftSide />
@@ -20,13 +21,65 @@ const HomeLayout = () => {
                     <div className='top-btn-container'>
                         <div><h5>For You</h5></div>
                         <div><h5>Following</h5></div>
-                        </div>
-                        </div>
                     </div>
-                   
-                 <RightSide />
-               </section>
-                )
+                </div>
+                <div className='whats-happening-field'>
+                    <div >
+                        <img src='https://picsum.photos/536/354'/>
+                       
+                    </div>
+                    <div className='whats-happening-control'> <button className='btn everone-btn'>Every One</button>
+                    <div className='text-field'>
+                        <textarea rows={1} placeholder="what's happening..."/>
+                    </div>
+                    <button className='reply-btn'>Every One Can Reply</button>
+                    </div>
+                </div>
+                <div className='feed-container'>
+                    <div className='feed-block'>
+
+                        {data.map((items, index) => (
+                            <>
+                                <div className='account-caption'>
+                                    <div className='user-img'>
+                                        <img src={items.user_img} alt='user-img' />
+                                    </div>
+                                    <div className='post-body'>
+
+                                        {data.map((item, index) => (
+                                            <>
+                                                <div className='caption'>
+                                                    <h5>{item.name} <span style={{ color: "gray" }}>@{item.user_name}
+                                                    </span> <small>June 01</small> </h5>
+                                                    <p>{item.caption}</p>
+                                                </div>
+                                                <div className='post-img'>
+                                                    <img src={item.img} alt='post-img' />
+                                                </div>
+
+                                                <div className='like-comment-btn'>
+                                                    <span><BsChat /><small>113</small></span>
+                                                    <span><AiOutlineRetweet /> <small>797</small></span>
+                                                    <span> <AiOutlineHeart onClick={() =>{ setLike(like + 1) }} style={{ color: (like > 0) ? "red" : "none" }} /> <small>{like}</small></span>
+                                                    <MdIosShare />
+                                                </div>
+                                            </>
+                                        )
+                                        )}
+                                    </div>
+                                </div>
+                            </>
+                        ))}
+                        
+                    </div>
+                </div>
+
+            </div>
+
+
+            <RightSide />
+        </section>
+    )
 }
 
-                export default HomeLayout
+export default HomeLayout
